8cd9de66cf7b7d943cc76014b263f2f906af7c55
laxyy69 <vrekmakerq@gmail.com>
2024-03-08 11:16:23 +0200

    client: Show status information in root

--- client/public/index.js
+++ client/public/index.js
@@ -1,13 +1,16 @@
 
-var session_id = localStorage.getItem("session_id");
+let session_id = localStorage.getItem("session_id");
+let status_h1 = document.getElementById("status");
 
 console.log("Session ID:", session_id);
 
 if (session_id)
 {
-    var socket = new WebSocket("wss://" + window.location.host);
+    let socket = new WebSocket("wss://" + window.location.host);
+    status_h1.innerHTML = "Connecting to server...";
 
     socket.addEventListener("open", (event) => {
+        status_h1.innerHTML = "Connected to server. Waiting for server";
         var packet = {
             type: "session",
             id: session_id
@@ -20,11 +23,14 @@ if (session_id)
         const packet = JSON.parse(event.data);
         if (packet.type == "session")
         {
+            status_h1.innerHTML = "Good, switching to /app";
             window.location.href = "/app";
         }
         else
         {
             console.log(packet);
+            if (packet.type === "error")
+                status_h1.innerHTML = "Error: " + packet.error_msg;
             localStorage.removeItem("session_id");
             window.location.href = "/login";
         }
@@ -32,10 +38,11 @@ if (session_id)
 
     socket.addEventListener("error", (event) => {
         console.log("error:", event.data);
+        status_h1.innerHTML = "Socket error: " + event.data;
     })
 
     socket.addEventListener("close", (event) => {
-
+        status_h1.innerHTML = "Lost connection to server.";
     });
 
     // window.location.href = "/app";
