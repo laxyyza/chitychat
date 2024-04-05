61e56b95117e9b7089213768c9172ccc880687cb
Your Name <you@example.com>
2024-04-05 13:31:24 +0200

    server_db.h: Indent for readability

--- server/include/server_db.h
+++ server/include/server_db.h
@@ -6,118 +6,118 @@
 
 #include <libpq-fe.h>
 
-#define DB_USERNAME_MAX 50
-#define DB_DISPLAYNAME_MAX 50
-#define DB_MESSAGE_MAX  512
-#define DB_TIMESTAMP_MAX 64
-#define DB_BIO_MAX 256
-#define DB_DESC_MAX 256
-#define DB_PASSWORD_MAX 50
-#define DB_PFP_HASH_MAX SERVER_HASH256_STR_SIZE
-#define DB_PFP_NAME_MAX NAME_MAX
-#define DB_MIME_TYPE_LEN 32 
+#define DB_USERNAME_MAX     50
+#define DB_DISPLAYNAME_MAX  50
+#define DB_MESSAGE_MAX      512
+#define DB_TIMESTAMP_MAX    64
+#define DB_BIO_MAX          256
+#define DB_DESC_MAX         256
+#define DB_PASSWORD_MAX     50
+#define DB_PFP_HASH_MAX     SERVER_HASH256_STR_SIZE
+#define DB_PFP_NAME_MAX     NAME_MAX
+#define DB_MIME_TYPE_LEN    32 
 
-#define DB_CONNINTO_LEN 256
+#define DB_CONNINTO_LEN     256
 
 typedef struct 
 {
-    u32 user_id;
-    char username[DB_USERNAME_MAX];
-    char displayname[DB_DISPLAYNAME_MAX];
-    char bio[DB_BIO_MAX];
-    u8   hash[SERVER_HASH_SIZE];
-    u8   salt[SERVER_SALT_SIZE];
-    char created_at[DB_TIMESTAMP_MAX];
-    char pfp_hash[DB_PFP_NAME_MAX];
-    i32 flags;
-    bool online;
+    u32     user_id;
+    char    username[DB_USERNAME_MAX];
+    char    displayname[DB_DISPLAYNAME_MAX];
+    char    bio[DB_BIO_MAX];
+    u8      hash[SERVER_HASH_SIZE];
+    u8      salt[SERVER_SALT_SIZE];
+    char    created_at[DB_TIMESTAMP_MAX];
+    char    pfp_hash[DB_PFP_NAME_MAX];
+    i32     flags;
+    bool    online;
 } dbuser_t;
 
 typedef struct 
 {
-    u32 group_id;
-    u32 owner_id;
-    char displayname[DB_DISPLAYNAME_MAX];
-    char desc[DB_DESC_MAX];
-    i32 flags;
+    u32     group_id;
+    u32     owner_id;
+    char    displayname[DB_DISPLAYNAME_MAX];
+    char    desc[DB_DESC_MAX];
+    i32     flags;
 } dbgroup_t;
 
 typedef struct 
 {
-    u32 user_id;
-    u32 group_id;
-    char join_date[DB_TIMESTAMP_MAX];
-    i32 flags;
+    u32     user_id;
+    u32     group_id;
+    char    join_date[DB_TIMESTAMP_MAX];
+    i32     flags;
 } dbgroup_member_t;
 
 typedef struct 
 {
-    u32 msg_id;
-    u32 user_id;
-    u32 group_id;
-    char content[DB_MESSAGE_MAX];
-    char timestamp[DB_TIMESTAMP_MAX];
-    char* attachments;
-    bool  attachments_inheap;
-    json_object* attachments_json;
-    i32 flags;
-    i32 parent_msg_id;
+    u32     msg_id;
+    u32     user_id;
+    u32     group_id;
+    char    content[DB_MESSAGE_MAX];
+    char    timestamp[DB_TIMESTAMP_MAX];
+    char*           attachments;
+    bool            attachments_inheap;
+    json_object*    attachments_json;
+    i32     flags;
+    i32     parent_msg_id;
 } dbmsg_t;
 
 typedef struct 
 {
-    char hash[DB_PFP_HASH_MAX];
-    char name[DB_PFP_NAME_MAX];
-    char mime_type[DB_MIME_TYPE_LEN];
-    size_t size;
-    i32 ref_count;
-    i32 flags;
+    char    hash[DB_PFP_HASH_MAX];
+    char    name[DB_PFP_NAME_MAX];
+    char    mime_type[DB_MIME_TYPE_LEN];
+    size_t  size;
+    i32     ref_count;
+    i32     flags;
 } dbuser_file_t;
 
 typedef struct 
 {
-    char* schema;
-    size_t schema_len;
+    char*   schema;
+    size_t  schema_len;
 
-    char* insert_user;
-    size_t insert_user_len;
-    char* select_user;
-    size_t select_user_len;
-    char* delete_user;
-    size_t delete_user_len;
+    char*   insert_user;
+    size_t  insert_user_len;
+    char*   select_user;
+    size_t  select_user_len;
+    char*   delete_user;
+    size_t  delete_user_len;
 
-    char* insert_group;
-    size_t insert_group_len;
-    char* select_group;
-    size_t select_group_len;
-    char* delete_group;
-    size_t delete_group_len;
+    char*   insert_group;
+    size_t  insert_group_len;
+    char*   select_group;
+    size_t  select_group_len;
+    char*   delete_group;
+    size_t  delete_group_len;
 
-    char* insert_groupmember;
-    size_t insert_groupmember_len;
-    char* select_groupmember;
-    size_t select_groupmember_len;
-    char* delete_groupmember;
-    size_t delete_groupmember_len;
+    char*   insert_groupmember;
+    size_t  insert_groupmember_len;
+    char*   select_groupmember;
+    size_t  select_groupmember_len;
+    char*   delete_groupmember;
+    size_t  delete_groupmember_len;
 
-    char* insert_msg;
-    size_t insert_msg_len;
-    char* select_msg;
-    size_t select_msg_len;
-    char* delete_msg;
-    size_t delete_msg_len;
+    char*   insert_msg;
+    size_t  insert_msg_len;
+    char*   select_msg;
+    size_t  select_msg_len;
+    char*   delete_msg;
+    size_t  delete_msg_len;
 
-    char* update_user;
-    size_t update_user_len;
+    char*   update_user;
+    size_t  update_user_len;
 
-    char* insert_userfiles;
-    size_t insert_userfiles_len;
+    char*   insert_userfiles;
+    size_t  insert_userfiles_len;
 } server_db_commands_t;
 
 typedef struct 
 {
-    PGconn*                 conn;
-    const server_db_commands_t*   cmd;
+    PGconn*                     conn;
+    const server_db_commands_t* cmd;
 } server_db_t;
 
 bool        server_init_db(server_t* server);
@@ -146,7 +146,7 @@ dbmsg_t*    server_db_get_msg(server_db_t* db, u32 msg_id);
 dbmsg_t*    server_db_get_msgs_from_group(server_db_t* db, u32 group_id, u32 limit, u32 offset, u32* n);
 bool        server_db_insert_msg(server_db_t* db, dbmsg_t* msg);
 
-dbuser_file_t*     server_db_select_userfile(server_db_t* db, const char* hash);
+dbuser_file_t*      server_db_select_userfile(server_db_t* db, const char* hash);
 i32                 server_db_select_userfile_ref_count(server_db_t* db, const char* hash);
 bool                server_db_insert_userfile(server_db_t* db, dbuser_file_t* file);
 bool                server_db_delete_userfile(server_db_t* db, const char* hash);
