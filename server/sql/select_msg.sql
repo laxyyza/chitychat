4213f0699590e13bb7b10f33ddf512969ada89f0
Your Name <you@example.com>
2024-04-18 00:34:55 +0200

    select_msg.sql: Remove JOIN

--- server/sql/select_msg.sql
+++ server/sql/select_msg.sql
@@ -1,6 +1,5 @@
 SELECT m.*
 FROM Messages m
-JOIN Groups g ON m.group_id = g.group_id
 WHERE m.group_id = $1::int OR m.msg_id = $2::int
 ORDER BY m.timestamp DESC, m.msg_id DESC
 LIMIT $3::int OFFSET $4::int;
