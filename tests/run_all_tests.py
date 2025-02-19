4fa331a891775d32edf9ea8fcea6371f8eb498d4
laxyyza <laxyy420@proton.me>
2025-02-19 11:04:46 +0200

    tests: Get user info and user groups

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -5,6 +5,8 @@ import sys
 
 import register
 import session_login
+import user_info
+import user_groups
 
 username = "test3"
 displayname = "Test"
@@ -12,6 +14,9 @@ password = "test_pass"
 
 def good(msg: str) -> None:
     print(f"GOOD: {msg}.")
+
+def info(msg: str) -> None:
+    print(f"INFO: {msg}")
 
 def bad(msg: str) -> None:
     print(f"BAD: {msg}.")
@@ -39,14 +44,27 @@ Then login with invalid session ID zero.
 async def test_session_login(session: dict) -> None:
     await session_login.run(session)
 
+    real_id = session["id"]
+
     try:
         # Try invalid session ID 0
         session["id"] = 0
         await session_login.run(session)
     except Exception:
         good("Got error when sending invalid session ID")
+        session["id"] = real_id
     else:
         raise RecursionError("Session ID zero worked?")
+
+async def test_user_info(session: dict) -> None:
+    user: dict = await user_info.run(session)
+    if user["username"] != username:
+        bad("user_info username is different!")
+    if user["displayname"] != displayname:
+        bad("user_info dispalyname is different!")
+    
+    groups: dict = await user_groups.run(session)
+    info(f"User:{user['user_id']} is in {len(groups['groups'])} groups")
 
 async def do_tests() -> None:
     # Register 
@@ -56,8 +74,9 @@ async def do_tests() -> None:
     await test_session_login(session)
 
     # Get user info.
+    await test_user_info(session)
 
-    # Create a private group.
+    # Create a private and public group.
 
     # Get user groups.
 
--- tests/user_info.py
+++ tests/user_info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Simple test to see if logging in and getting user groups works or not.
+# Simple test to see if logging in and getting user info works or not.
 
 from common import *
 
@@ -12,10 +12,10 @@ async def run(session: dict) -> dict:
         await test.connect()
         await test.request_wait("session", session)
 
-        groups: dict = await test.request_wait("client_groups", {"cmd": "client_groups"})
+        client_user_info:dict = await test.request_wait("client_user_info", {"cmd": "client_user_info"})
 
         await test.close()
-        return groups
+        return client_user_info 
     except Exception as e:
         await test.close()
         raise e
