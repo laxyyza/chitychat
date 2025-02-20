561dae09e76b5a3981fe81502f61818a4cc60e83
laxyyza <laxyy420@proton.me>
2025-02-20 13:40:27 +0200

    Restore server/sql/delete_msg

--- server/sql/delete_msg.sql
+++ server/sql/delete_msg.sql
@@ -4,15 +4,9 @@
 -- Delete message if only msg.user_id = $2 OR user is group owner.
 -- Return group_id and attachments
 
---DELETE FROM Messages m
---USING Groups g
---WHERE m.msg_id = $1::int
-    --AND m.group_id = g.group_id
-    --AND (m.user_id = $2::int OR g.owner_id = $2::int) 
---RETURNING g.group_id, m.attachments;
-
-SELECT g.group_id, m.attachments
-FROM Messages m
-JOIN Groups g ON m.group_id = g.group_id
+DELETE FROM Messages m
+USING Groups g
 WHERE m.msg_id = $1::int
-    AND (m.user_id = $2::int OR g.owner_id = $2::int);
+    AND m.group_id = g.group_id
+    AND (m.user_id = $2::int OR g.owner_id = $2::int) 
+RETURNING g.group_id, m.attachments;
