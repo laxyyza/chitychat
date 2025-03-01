a8f688226ca1fffe8b8f36e50bfc75c7ed142c48
laxyyza <laxyy420@proton.me>
2025-03-01 13:28:03 +0200

    server/http_post: Fix crash with random HTTP POST

--- server/src/server_http_post.c
+++ server/src/server_http_post.c
@@ -5,8 +5,16 @@
  * Currently only the chat-backend will handle HTTP POST requests.
  */
 
-void 
+enum client_recv_status
 server_handle_http_post(eworker_t* th, client_t* client, const http_t* http)
 {
+    if (http_get_header(http, "Upload-Token") == NULL)
+    {
+        server_http_resp_error(client, 401, "Unauthorized");
+        return RECV_DISCONNECT;
+    }
+
     server_handle_user_upload(th, client, http);
+
+    return RECV_OK;
 }
