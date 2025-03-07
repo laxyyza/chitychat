bb4fbab7e66d93057628866449500c7f9c40ec79
laxyyza <laxyy420@proton.me>
2025-03-07 07:53:01 +0200

    scripts/run.sh: Forward SIGTERM and SIGINT signals to child

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,16 @@
 # By running it as a child process, I can send SIGSEGV if the server stalls,
 # ensuring a proper core dump for debugging.
 
-bin/chitychat -R $@
+bin/chitychat -R $@ &
+pid=$!
+
+forward_signal() {
+    kill -$1 $pid
+}
+
+trap 'forward_signal SIGTERM' SIGTERM
+trap 'forward_signal SIGINT' SIGINT
+
+wait $pid
+
+exit $?
