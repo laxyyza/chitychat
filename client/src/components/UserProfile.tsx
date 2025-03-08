e356fb49b095bc5c1eae5f686c1783f8ce5627ed
laxyyza <laxyy420@proton.me>
2025-03-08 19:02:56 +0200

    client/UserProfile: Dont wrap username and displayname

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -12,13 +12,15 @@ const UserProfile = ({ user }: Prop) => {
             <div className="rounded-4xl bg-white w-13 h-13 m-1 flex">
                 <UserIcon user={user}></UserIcon>
             </div>
-            <div className="flex-1 p-0 ml-1">
-                <div className="font-bold text-left text-lg">
+            <div className="flex-1 min-w-0 p-0 ml-1">
+                <div className="font-bold text-left text-lg text-nowrap text-ellipsis overflow-hidden">
                     {user.displayname}
                 </div>
-                <div className="text-[14px]">{user.username}</div>
+                <div className="text-[14px] text-nowrap text-ellipsis overflow-hidden">
+                    {user.username}
+                </div>
             </div>
-            <CiSettings className="m-1 bg-black" size="32"></CiSettings>
+            <CiSettings className="g-black m-auto" size="32"></CiSettings>
         </div>
     );
 };
