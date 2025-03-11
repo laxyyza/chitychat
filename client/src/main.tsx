8196faae98f214ba60d95eb8da6cad0a9f358ddc
laxyyza <laxyy420@proton.me>
2025-03-11 17:45:46 +0200

    Initial loading page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import MainApp from './App.tsx';
 import { AppProvider } from './components/AppProvider.tsx';
 import Login from './components/Login.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Loading from './components/Loading.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
                 <Routes>
                     <Route path="/app" element={<MainApp />}></Route>
                     <Route path="/login" element={<Login />}></Route>
+                    <Route path="/" element={<Loading />}></Route>
                 </Routes>
             </BrowserRouter>
         </AppProvider>
