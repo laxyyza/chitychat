a8a9419a4f54ea8260034a21000310cd64d26728
laxyyza <laxyy420@proton.me>
2025-03-11 17:51:05 +0200

    client/Loading: Make it bigger and 2 more emotes

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 const Loading = () => {
     const [animation, setAnimation] = useState('animate-bounce');
-    const emotes = ['>_<', ':3', ':)', ':(', '^_^'];
+    const emotes = ['>_<', ':3', ':)', ':(', '^_^', '>:(', '>:)'];
     const [emote, setEmote] = useState(emotes[0]);
 
     useEffect(() => {
@@ -10,34 +10,34 @@ const Loading = () => {
             setAnimation((prev) =>
                 prev === 'animate-spin' ? '' : 'animate-spin'
             );
-        }, 2000);
+        }, 3000);
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setEmote(emotes[Math.floor(Math.random() * emotes.length)]);
-        }, 6000);
+        }, 1200);
         return () => clearInterval(interval);
     }, []);
 
     return (
         <>
             <div className="flex justify-center items-center w-screen h-screen bg-black">
-                <h1 className="text-white text-5xl text-cente m-2 animate-pulse">
+                <h1 className="text-white text-6xl text-cente m-2 animate-pulse">
                     Chity Chat...
                 </h1>
                 <div className="animate-bounce">
                     <div
                         className={
-                            'w-13 h-13 rounded-full border-blue-500 bg-blue-500 border-5 font-bold text-2xl text-center ' +
+                            'w-14 h-14 rounded-full border-blue-500 bg-blue-500 border-5 font-bold text-3xl text-center ' +
                             animation
                         }
                     >
                         {emote}
                     </div>
                 </div>
-                <span className="ml-4 text-xs top-0 bottom-0 text-white">
+                <span className="ml-4 top-0 bottom-0 text-white">
                     Connecting...
                 </span>
             </div>
