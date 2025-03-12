8e7e9ee5d82b7bf8508876f84ad88cecd520e60d
laxyyza <laxyy420@proton.me>
2025-03-12 16:54:19 +0200

    client/App: Initial getting client user info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,41 @@ import SideBar from './components/SideBar';
 import MainContent from './components/MainContent';
 import MemberList from './components/MemberList';
 import { Action, useApp } from './components/AppProvider';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import websocketClient from './services/websocketClient';
+import useWebsocket from './components/WebSocket';
 
 function MainApp() {
-    const { app, dispatch } = useApp();
-    const [test, setTest] = useState(0);
+    const { dispatch } = useApp();
+    // const [test, setTest] = useState(0);
     const navigate = useNavigate();
 
+    const { send } = useWebsocket((event) => {
+        const packet = JSON.parse(event.data);
+        const cmd = packet['cmd'];
+
+        switch (cmd) {
+            case 'client_user_info': {
+                dispatch({
+                    type: Action.SET_LOGIN_USER,
+                    payload: {
+                        id: packet.user_id,
+                        username: packet.username,
+                        displayname: packet.displayname,
+                        created_at: packet.create_at,
+                        about_me: packet.bio,
+                        pfp: ''
+                    }
+                });
+            }
+        }
+    });
+
     useEffect(() => {
+        send({ cmd: 'client_user_info' });
+        send({ cmd: 'client_groups' });
+
         // app.hubs.set(
         //     1,
         //     new Hub(
@@ -30,25 +55,25 @@ function MainApp() {
             }
         });
 
-        app.login_user = {
-            id: 1,
-            username: 'username',
-            displayname: 'Display Name',
-            pfp: 'https://www.oola.com/wp-content/uploads/2022/07/communityIcon_x4lqmqzu1hi81.jpeg',
-            created_at: '15 January 2025, 12:30 PM',
-            about_me: 'About me'
-        };
-        app.users.set(app.login_user.id, app.login_user);
-        app.users.set(2, {
-            id: 2,
-            username: 'laxyyza',
-            displayname: 'Laxyy',
-            pfp: 'https://i.pinimg.com/236x/1a/ae/bc/1aaebcf79c6ef766603c655b3bef104f.jpg',
-            created_at: '?',
-            about_me: ''
-        });
+        // app.login_user = {
+        //     id: 1,
+        //     username: 'username',
+        //     displayname: 'Display Name',
+        //     pfp: 'https://www.oola.com/wp-content/uploads/2022/07/communityIcon_x4lqmqzu1hi81.jpeg',
+        //     created_at: '15 January 2025, 12:30 PM',
+        //     about_me: 'About me'
+        // };
+        // app.users.set(app.login_user.id, app.login_user);
+        // app.users.set(2, {
+        //     id: 2,
+        //     username: 'laxyyza',
+        //     displayname: 'Laxyy',
+        //     pfp: 'https://i.pinimg.com/236x/1a/ae/bc/1aaebcf79c6ef766603c655b3bef104f.jpg',
+        //     created_at: '?',
+        //     about_me: ''
+        // });
 
-        dispatch({ type: Action.ADD_HUB, payload: 'Test' });
+        // dispatch({ type: Action.ADD_HUB, payload: 'Test' });
 
         // app.hubs.get(1)?.memberIDs.push(2);
         // app.textChannels.set(1, {
@@ -99,8 +124,6 @@ function MainApp() {
         //     name: 'Text Channels',
         //     channelIDs: [3]
         // });
-
-        setTest(test + 1);
 
         return () => websocketClient.onStateChange(undefined);
     }, []);
