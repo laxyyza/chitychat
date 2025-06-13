06bafb71b1da6c93894841fe2b8794b18dfdd541
laxyyza <laxyy420@proton.me>
2025-06-13 12:50:39 +0200

    scripts: Delete build-go-docker.sh, move it into build-docker.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 
-ARG=$1
+ARG=${1-all}
 BUILD_DIR=${BUILD_DIR:=build}
 GO_DIR=${GO_DIR:=backend/services/go}
 SERVICES_OUT=${SERVICES_OUT:=$BUILD_DIR/backend/services}
 FRONTEND_DIR=${FRONTEND_DIR:=client}
-
-echo "BUILD_DIR: ${BUILD_DIR}"
+BUILD_DOCKER=${BUILD_DOCKER:=./scripts/build-docker.sh}
 
 function build_go() 
 {
@@ -34,18 +33,52 @@ function build_server()
     mkdir -p $SERVICES_OUT
 }
 
-if [[ $ARG == "go" ]]; then
-    build_go
-elif [[ $ARG == "frontend" ]]; then
-    build_frontend
-elif [[ $ARG == "server" ]]; then
-    build_server
-elif [[ $ARG == "backend" ]]; then
-    build_server
-    build_go
-else
-    build_server
-    build_go
-    build_frontend
-fi
+function print_help()
+{
+    echo "
+Usage:
+    $0 [COMMAND]
 
+COMMANDS:
+    cc_server   -   Build 'cc_server' (backend/server/)
+    backend     -   Build backend 'cc_server' + Go services (backend/)
+    frontend    -   Build frontend (client/)
+    go          -   BUild Go backend services (backend/services/go/)
+    image       -   Run '$BUILD_DOCKER' with \$2 as argument
+    help        -   Print this message
+    default     -   Build backend and frontend (default when no arguments)
+    "
+    exit 1
+}
+
+case $ARG in
+    cc_server)
+        build_server
+        ;;
+    backend)
+        build_server
+        build_go
+        ;;
+    frontend)
+        build_frontend
+        ;;
+    go)
+        build_go
+        ;;
+    image)
+        shift 1
+        exec ./scripts/build-docker.sh $@
+        ;;
+    default)
+        build_server
+        build_go
+        build_frontend
+        ;;
+    help)
+        print_help
+        ;;
+    *)
+        echo "Unkown service: '$ARG'"
+        exit -1
+        ;;
+esac
